fe187f1e43ae572a5e2e14b020268eace9279441
Parth Shelgaonkar <ps@parthshel.com>
2016-09-17 13:03:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
-var express = require('express')
-, stylus = require('stylus')
-, nib = require('nib')
-//, sio = require('..//..//lib//socket.io'); //Original
-, sio = require('socket.io');                //Updated
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-	
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+//Function is a router between server.js and client for requests
+module.exports = function(app)
+{
+  app.get('/',function(req,res){
+    res.render('index.html');
   });
-
-});
-
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+  app.get('/about',function(req,res){
+    res.render('about.html');
+  });
+}
