cd35cbf7e8c2ffcd213951438ba9c01a59f8eaa9
Parth Shelgaonkar <ps@parthshel.com>
2016-09-17 13:07:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Function is a router between server.js and client for requests
-module.exports = function(app)
+/*module.exports = function(app)
 {
   app.get('/',function(req,res){
     res.render('index.html');
@@ -7,4 +7,16 @@ module.exports = function(app)
   app.get('/about',function(req,res){
     res.render('about.html');
   });
-}
+}*/
+
+var express = require('express')
+, stylus = require('stylus')
+, nib = require('nib')
+//, sio = require('..//..//lib//socket.io'); //Original
+, sio = require('socket.io');                //Updated
+
+//app.listen(3000, function () {            //Original
+app.listen(process.env.port, function () {  //Updated
+  var addr = app.address();
+  console.log('   app listening on http://' + addr.address + ':' + addr.port);
+});
